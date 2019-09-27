6d5a0c38d76e84ac82de358f724904d35dfb541a
rafiyahya <rafiblue6@gmail.com>
2019-09-28 00:08:58 +0800

    Mengubah home dan menghapus user_guide

---
+++ application/views/v_home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Selamat Datang di PPDB Online</title>
+</head>
+<body>
+
+<h1>Selamat Datang di PPDB Online</h1>
+
+</body>
+</html>
