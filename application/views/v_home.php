4aaf3473842b1bc8602730f4394ee18a209b954b
rafiyahya <rafiblue6@gmail.com>
2019-09-28 16:21:24 +0800

    Instalasi bootstrap dan penambahan navbar

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -1,14 +1,11 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Selamat Datang di PPDB Online</title>
-</head>
-<body>
-
-<h1>Selamat Datang di PPDB Online</h1>
-
-</body>
+  <head>
+	<?php include 'header.php'; ?>
+  </head>
+  <body>
+	<?php include 'navbar.php'; ?>
+    <h1>Hello, world!</h1>
+	<?php include 'footer.php'; ?>
+  </body>
 </html>
--- application/views/v_pendaftaran.php
+++ application/views/v_pendaftaran.php
@@ -1,37 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Selamat Datang di PPDB Online</title>
+	<?php include 'header.php'; ?>
 </head>
 <body>
-	<h2>Masukkan data anda</h2>
+	<?php include 'navbar.php'; ?>
+<div class="container">
+	<div class="col-sm-5">
+	<h3>Masukkan data Siswa</h3>
 	<form action="pendaftaran/input_data" method="POST">
+		<div class="form-group">
 		<label>Nama Siswa : </label>
-		<input type="text" name="namaSiswa"><br>
+		<input class="form-control" type="text" name="namaSiswa" required>
 		<label>NISN : </label>
-		<input type="text" name="nisn"><br>
+		<input class="form-control" type="text" name="nisn" required>
 		<label>Jenis Kelamin : </label>
-		<select name="jenisKelamin">
+		<select class="form-control" name="jenisKelamin">
 			<option value="1">Laki-Laki</option>
 			<option value="2">Perempuan</option>
-		</select><br>
+		</select>
 		<label>Tanggal Lahir</label>
-		<input type="date" name="tanggalLahir"><br>
+		<input class="form-control" type="date" name="tanggalLahir" required>
 		<label>Alamat : </label>
-		<input type="text" name="alamat"><br>
+		<input class="form-control" type="text" name="alamat" required>
 		<label>Asal Sekolah : </label>
-		<input type="text" name="asalSekolah"><br>
+		<input class="form-control" type="text" name="asalSekolah" required>
 		<label>Nama Ayah : </label>
-		<input type="text" name="namaAyah"><br>
+		<input class="form-control" type="text" name="namaAyah" required>
 		<label>Nama Ibu : </label>
-		<input type="text" name="namaIbu"><br>
+		<input class="form-control" type="text" name="namaIbu" required>
 		<label>Minat dan Bakat : </label>
-		<select name="minatBakat">
+		<select class="form-control" name="minatBakat">
 			<option value="1">Olahraga</option>
 			<option value="2">Kesenian</option>
 			<option value="2">Pramuka</option>
-		</select><br>
-		<input type="submit" name="sumbit" value="Simpan">
+		</select>
+		<button type="submit" class="btn btn-success mt-3">Simpan</button>
+	</div>
 	</form>
+	</div>
+</div>
+	<?php include 'footer.php'; ?>
 </body>
 </html>
